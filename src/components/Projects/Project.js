1634b8340b29bdef4d8e6fb511af83a996327e42
Guilherme Fleck Oliveira <guilhermefleckoliveira@gmail.com>
2020-06-11 13:59:54 -0300

    Integrated projects section with github api to retrieve my latest repos

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,28 +1,22 @@
 import React from 'react';
-import Eye from '../../img/Eye.svg';
 import './Project.scss';
 
 export default function Project({title, description, github_url, togglePreview}) {
     return (
         <>
-            <div className="Project purple-gradient">
+            <div className="Project">
                 <div className="Content">
                     <h3 className="title">{title}</h3>
                     <p className="description" dangerouslySetInnerHTML={{
                         __html: description
                     }}></p>
-                    <a 
-                        href={github_url} 
-                        className="btn github"
-                        target="_blank"
-                        rel="noopener noreferrer"
+                    <button 
+                        className="btn purple-gradient"
+                        onClick={() => togglePreview(title, { description, github_url })}
                     >
-                        Check it on Github
-                    </a>
+                        See More
+                    </button>
                 </div>
-                <button className="PreviewButton btn" title="Preview" onClick={() => togglePreview(title, { description, github_url })}>
-                    <img src={Eye} alt="Preview"/>
-                </button>
             </div>
         </>
     )
